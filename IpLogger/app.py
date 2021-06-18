3abe5a55d77c8e8fb560c54e7639680774e42413
AhmedWaleed22 <ahmedwaleedaboelamayem68@gmail.com>
2021-06-18 13:26:05 +0200

    New Updates In Key Logger

--- IpLogger/app.py
+++ IpLogger/app.py
@@ -1,32 +1,23 @@
 from flask import Flask, render_template, request, redirect
 from termcolor import colored
 import requests as rq
+import urllib.request
 
 app = Flask(__name__)
 
+def getIp():
+    req = urllib.request.urlopen("http://dynupdate.no-ip.com/ip.php")
+    ip = req.read()
+    return ip.decode('utf-8')
+
 @app.route("/")
 def index():
-    return render_template("indexenc.html")
+    ipaddr = getIp()
+    serviceCheck = rq.get(f"https://proxycheck.io/v2/{ipaddr}?vpn=1&asn=1").json()
 
-@app.route("/nextstep/", methods=["POST"])
-def nextstep():
-    ipaddr = request.form.get("ip")
-    servicesList = []
-    serviceCheck = rq.get(f"https://vpnapi.io/api/{ipaddr}?key=F9J3K1V02MFO1C93KA7B").json()
+    print(colored(f"[*] FOUND IP ADDRESS --- {serviceCheck}", "cyan", attrs=["bold"]))
 
-    if serviceCheck['security']['vpn'] == True:
-        servicesList.append('VPN')
-    elif serviceCheck['security']['proxy'] == True:
-        servicesList.append('PROXY')
-    elif serviceCheck['security']['tor'] == True:
-        servicesList.append('TOR')
-
-    if len(servicesList) < 1:
-        services = "REAL IP ADDRESS"
-    else:
-        services = ",".join(servicesList)
-    print(colored(f"[*] FOUND IP ADDRESS --- ({services}) {ipaddr}", "cyan", attrs=["bold"]))
-    return "An Error Happend!"
+    return "Internal Server Error", 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=80)
+    app.run(debug=True, port=8080)
